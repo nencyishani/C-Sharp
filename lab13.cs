87da6698d1c0c22c9582520e52beb9c3ce96d28a
nencyishani <nency.ishani127387@marwadiuniversity.ac.in>
2026-03-16 10:06:01 +0530

    Add files via upload

---
+++ lab13.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Rectangle
+{
+    double length, width;
+
+    
+    public Rectangle()
+    {
+        length = 0;
+        width = 0;
+    }
+
+    
+    public Rectangle(double l, double w)
+    {
+        length = l;
+        width = w;
+    }
+
+    public double Area()
+    {
+        return length * width;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Length: " + length + " Width: " + width);
+        Console.WriteLine("Area: " + Area());
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = new Rectangle(10, 5);
+
+        r1.Display();
+        r2.Display();
+    }
+}
