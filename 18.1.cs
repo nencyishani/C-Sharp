5a64b0d2a40b3d88987d069bac6b04f8dd82b71f
nencyishani <nency.ishani127387@marwadiuniversity.ac.in>
2026-04-07 08:17:01 +0530

    Add files via upload

---
+++ 18.1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int a = 10;
+            int b = 0;
+            int c = a / b;   // This will cause an error
+            Console.WriteLine("Result: " + c);
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine("Error: Cannot divide by zero.");
+        }
+
+        Console.WriteLine("Program continues...");
+    }
+}
