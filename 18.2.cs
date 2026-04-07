6680293d42a4bcc06c8f81da1f675267c214a687
nencyishani <nency.ishani127387@marwadiuniversity.ac.in>
2026-04-07 08:17:37 +0530

    Add files via upload

---
+++ 18.2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int a = 20;
+            int b = 0;
+            int c = a / b;
+            Console.WriteLine("Result: " + c);
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine("Error: Cannot divide by zero.");
+        }
+        finally
+        {
+            Console.WriteLine("Finally block always executes.");
+        }
+    }
+}
